56590002a71cf5b11fe73ffad4d8666bdc036a6c
filatok <olenkaul@gmail.com>
2026-03-27 16:23:02 +0200

    initial commit

---
+++ src/test/java/gr/filatok/lybrary/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.filatok.lybrary.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
